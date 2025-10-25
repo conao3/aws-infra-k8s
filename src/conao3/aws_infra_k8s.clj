5b91d557c0d034940220ace8eff8b0bdc7b98274
Naoya Yamashita <conao3@gmail.com>
2025-10-25 17:33:08 +0900

    refactor

--- src/conao3/aws_infra_k8s.clj
+++ src/conao3/aws_infra_k8s.clj
@@ -8,21 +8,20 @@
 (defn -main [& args]
   (let [env "dev"
         prefix (format "%s-%s" env "k8s")
+        param {:env env :prefix prefix}
         [command & rest] args]
     (case command
       "deploy" (let [target (first rest)]
                  (case target
-                   "network" (c.network/deploy {:env env
-                                                :prefix prefix
-                                                :vpc "10.0.0.0/16"
-                                                :subnet-pub-a "10.0.0.0/24"
-                                                :subnet-pri-a "10.0.10.0/24"
-                                                :subnet-pub-c "10.0.20.0/24"
-                                                :subnet-pri-c "10.0.30.0/24"})
-                   "routing" (c.routing/deploy {:env env
-                                                :prefix prefix})
-                   "security-group" (c.security-group/deploy {:env env
-                                                              :prefix prefix})
+                   "network" (c.network/deploy (merge
+                                                param
+                                                {:vpc "10.0.0.0/16"
+                                                 :subnet-pub-a "10.0.0.0/24"
+                                                 :subnet-pri-a "10.0.10.0/24"
+                                                 :subnet-pub-c "10.0.20.0/24"
+                                                 :subnet-pri-c "10.0.30.0/24"}))
+                   "routing" (c.routing/deploy param)
+                   "security-group" (c.security-group/deploy param)
                    "all" (do
                            (-main "deploy" "network")
                            (-main "deploy" "routing")))))))
