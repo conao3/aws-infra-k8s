76fbaaffd984e876b63e164b62316bd5cb14538a
conao3 <conao3@gmail.com>
2026-01-29 00:29:31 +0900

    update

--- nixos/hosts/vm.nix
+++ nixos/hosts/vm.nix
@@ -8,8 +8,14 @@
 
   virtualisation.qemu.options = ["-nographic"];
   virtualisation.qemu.consoles = ["ttyAMA0"];
-  boot.kernelParams = ["console=ttyAMA0"];
+  boot.kernelParams = [
+    "console=ttyAMA0"
+    "systemd.unified_cgroup_hierarchy=0"
+    "SYSTEMD_CGROUP_ENABLE_LEGACY_FORCE=1"
+  ];
   boot.loader.timeout = 0;
 
   services.xserver.enable = false;
+
+  systemd.services.systemd-udevd.serviceConfig.SystemCallFilter = "";
 }
