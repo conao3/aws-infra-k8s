24d8fb429ad92ac6267764b99ca8f0c07d19770f
conao3 <conao3@gmail.com>
2026-02-02 00:46:08 +0900

    enable k3s

--- nixos/nixos-configuration.nix
+++ nixos/nixos-configuration.nix
@@ -5,4 +5,16 @@
     curl
     vim
   ];
+
+  services.k3s = {
+    enable = true;
+    role = "server";
+    extraFlags = toString [
+      "--disable=traefik" # use ALB instead
+      "--disable=servicelb" # use ALB/NLB instead
+      "--write-kubeconfig-mode=644" # allow non-root users to access kubeconfig
+    ];
+  };
+
+  networking.firewall.allowedTCPPorts = [6443]; # k8s API server
 }
