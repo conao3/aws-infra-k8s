ee02fe23bba950e23a6909f241de206f70279b25
conao3 <conao3@gmail.com>
2026-02-01 15:34:24 +0900

    add comment

--- nixos/hosts/ec2-image.nix
+++ nixos/hosts/ec2-image.nix
@@ -4,14 +4,56 @@
   lib,
   ...
 }: let
-  eiceAuthorizedKeysCommand = pkgs.writeShellScript "eice-authorized-keys" ''
-    set -euo pipefail
-    USER="$1"
-    TOKEN=$(${pkgs.curl}/bin/curl -X PUT -s http://169.254.169.254/latest/api/token -H "X-aws-ec2-metadata-token-ttl-seconds: 21600" 2>/dev/null || echo "")
-    if [ -n "$TOKEN" ]; then
-      ${pkgs.curl}/bin/curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/managed-ssh-keys/active-keys/"$USER" 2>/dev/null || true
-    fi
-  '';
+  # Ref: https://github.com/aws/aws-ec2-instance-connect-config
+  # Ref: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-connect-set-up.html
+  ec2-instance-connect-scripts = pkgs.stdenv.mkDerivation {
+    pname = "ec2-instance-connect-scripts";
+    version = "1.1.17";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "aws";
+      repo = "aws-ec2-instance-connect-config";
+      rev = "1.1.17";
+      sha256 = "sha256-EFJe8mj+Z7ls+Aq1fvP9Ll6fH/bYM6FYiF6hSNaXvhM=";
+    };
+
+    nativeBuildInputs = with pkgs; [makeWrapper];
+
+    patchPhase = ''
+      substituteInPlace src/bin/eic_curl_authorized_keys \
+        --replace-fail '/usr/bin/curl' '${pkgs.curl}/bin/curl' \
+        --replace-fail '/usr/bin/logger' '${pkgs.util-linux}/bin/logger' \
+        --replace-fail '/bin/echo' '${pkgs.coreutils}/bin/echo' \
+        --replace-fail '/usr/bin/head' '${pkgs.coreutils}/bin/head' \
+        --replace-fail '/bin/grep' '${pkgs.gnugrep}/bin/grep' \
+        --replace-fail '/bin/cat' '${pkgs.coreutils}/bin/cat' \
+        --replace-fail '/usr/bin/id' '${pkgs.coreutils}/bin/id' \
+        --replace-fail '/bin/mktemp' '${pkgs.coreutils}/bin/mktemp' \
+        --replace-fail '/bin/chmod' '${pkgs.coreutils}/bin/chmod' \
+        --replace-fail '/usr/bin/base64' '${pkgs.coreutils}/bin/base64' \
+        --replace-fail '/usr/bin/openssl' '${pkgs.openssl}/bin/openssl'
+
+      substituteInPlace src/bin/eic_parse_authorized_keys \
+        --replace-fail '/usr/bin/openssl' '${pkgs.openssl}/bin/openssl' \
+        --replace-fail '/usr/bin/logger' '${pkgs.util-linux}/bin/logger' \
+        --replace-fail '/bin/echo' '${pkgs.coreutils}/bin/echo' \
+        --replace-fail '/bin/mktemp' '${pkgs.coreutils}/bin/mktemp' \
+        --replace-fail '/bin/chmod' '${pkgs.coreutils}/bin/chmod' \
+        --replace-fail '/bin/cat' '${pkgs.coreutils}/bin/cat' \
+        --replace-fail '/bin/grep' '${pkgs.gnugrep}/bin/grep'
+
+      substituteInPlace src/bin/eic_run_authorized_keys \
+        --replace-fail '/usr/bin/timeout' '${pkgs.coreutils}/bin/timeout'
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin
+
+      install -m 755 src/bin/eic_run_authorized_keys $out/bin/
+      install -m 755 src/bin/eic_curl_authorized_keys $out/bin/
+      install -m 755 src/bin/eic_parse_authorized_keys $out/bin/
+    '';
+  };
 in {
   imports = [
     "${modulesPath}/virtualisation/amazon-image.nix"
@@ -20,8 +62,14 @@ in {
 
   ec2.hvm = true;
 
-  services.openssh.authorizedKeysCommand = lib.mkForce "${eiceAuthorizedKeysCommand} %u";
-  services.openssh.authorizedKeysCommandUser = "root";
+  users.users.ec2-instance-connect = {
+    isSystemUser = true;
+    group = "ec2-instance-connect";
+  };
+  users.groups.ec2-instance-connect = {};
+
+  services.openssh.authorizedKeysCommand = lib.mkForce "${ec2-instance-connect-scripts}/bin/eic_run_authorized_keys %u %f";
+  services.openssh.authorizedKeysCommandUser = "ec2-instance-connect";
 
   systemd.services.fetch-ec2-ssh-key = {
     description = "Fetch EC2 SSH key from metadata service";
